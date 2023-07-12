71ec05861fba21a86884f29581a0aa0950547260
Takeshi <orwellj@mail.ru>
2023-07-13 00:46:23 +0300

    add expense by categories and change TransactionCategory

--- server/apps/pockets/viewsets/transaction.py
+++ server/apps/pockets/viewsets/transaction.py
@@ -8,9 +8,10 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.request import Request
 from rest_framework.response import Response
 
-from ..models import Transaction
+from ..models import Transaction, TransactionCategory
 from ..models.querysets import TransactionQuerySet
 from ..serializers import (
+    TransactionCategoryTransactionSumSerializer,
     TransactionCreateSerializer,
     TransactionRetrieveSerializer,
     TransactionGlobalSerializer,
@@ -25,6 +26,8 @@ class TransactionViewSet(viewsets.ModelViewSet):
     def get_serializer_class(self) -> Type[serializers.ModelSerializer]:
         if self.action == 'total':
             serializer_class = TransactionGlobalSerializer
+        elif self.action == 'expenses_by_category':
+            serializer_class = TransactionCategoryTransactionSumSerializer
         elif self.action in {'create', 'update', 'partial_update'}:
             serializer_class = TransactionCreateSerializer
         else:
@@ -33,13 +36,17 @@ class TransactionViewSet(viewsets.ModelViewSet):
         return serializer_class
 
     def get_queryset(self) -> TransactionQuerySet:
-        return Transaction.objects.filter(
-            user=self.request.user,
-        ).select_related(
-            'category',
-        ).order_by(
-            '-transaction_date', '-id',
-        )
+        if self.action == "expenses_by_category":
+            queryset = TransactionCategory.objects.filter(
+                user=self.request.user,
+            ).annotate_with_transaction_sums()
+        else:
+            queryset = Transaction.objects.filter(
+                user=self.request.user,
+            ).select_related('category',).order_by(
+                '-transaction_date', '-id',
+            )
+        return queryset
 
     def get_object(self) -> Union[Transaction, dict[str, Decimal]]:
         if self.action == 'total':
@@ -54,3 +61,7 @@ class TransactionViewSet(viewsets.ModelViewSet):
     @action(methods=('GET',), detail=False, url_path='global')
     def total(self, request: Request, *args, **kwargs) -> Response:
         return super().retrieve(request, *args, **kwargs)
+
+    @action(methods=('GET',), detail=False, url_path='expenses-by-category')
+    def expenses_by_category(self, request: Request, *args, **kwargs) -> Response:
+        return super().list(request, *args, **kwargs)
--- server/apps/pockets/viewsets/transaction_category.py
+++ server/apps/pockets/viewsets/transaction_category.py
@@ -1,30 +1,17 @@
-from typing import Type
-
 from django.db.models import QuerySet
 from rest_framework import viewsets, serializers
-from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.request import Request
-from rest_framework.response import Response
 
 from ..models import TransactionCategory
 from ..serializers import (
     TransactionCategorySerializer,
-    TransactionCategoryTransactionSumSerializer,
 )
 
 
 class TransactionCategoryViewSet(viewsets.ModelViewSet):
     permission_classes = (IsAuthenticated,)
     http_method_names = ("get", "post",)
-
-    def get_serializer_class(self) -> Type[serializers.ModelSerializer]:
-        if self.action == 'list':
-            serializer_class = TransactionCategoryTransactionSumSerializer
-        else:
-            serializer_class = TransactionCategorySerializer
-
-        return serializer_class
+    serializer_class = TransactionCategorySerializer
 
     def get_queryset(self) -> QuerySet:
         queryset = TransactionCategory.objects.filter(
