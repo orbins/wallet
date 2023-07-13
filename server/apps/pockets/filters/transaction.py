dc7df1f1ca05218a935e0477d813f682d159f3f4
Takeshi <orwellj@mail.ru>
2023-07-13 13:46:28 +0300

    add sorting at transaction endpoint

--- server/apps/pockets/filters/transaction.py
+++ server/apps/pockets/filters/transaction.py
@@ -6,6 +6,13 @@ from ..models import Transaction
 class TransactionFilter(filters.FilterSet):
     month = filters.NumberFilter(field_name="transaction_date", lookup_expr="month")
     year = filters.NumberFilter(field_name="transaction_date", lookup_expr="year")
+    order_by = filters.OrderingFilter(
+        fields=(
+            ('-transaction_date', 'date'),
+            ('-amount', 'amount'),
+            ('category', 'category'),
+        )
+    )
 
     class Meta:
         model = Transaction
