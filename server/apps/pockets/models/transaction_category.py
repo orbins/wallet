0a0b2632fcc7bc0db33c738e4a53486633d8ca8f
Takeshi <orwellj@mail.ru>
2023-07-23 12:23:30 +0300

    change model constraint

--- server/apps/pockets/models/transaction_category.py
+++ server/apps/pockets/models/transaction_category.py
@@ -20,9 +20,7 @@ class TransactionCategory(models.Model):
     class Meta:
         verbose_name = 'Категория'
         verbose_name_plural = 'Категории'
-        constraints = [
-            models.UniqueConstraint(fields=['user', 'name'], name='unique_user_category_name')
-        ]
+        unique_together = ["user", "name"]
 
     def __str__(self) -> str:
         return f'{self.name}'
