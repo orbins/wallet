8d6f817bb61d0db28295f23f9ebbd4a7a9c83407
Takeshi <orwellj@mail.ru>
2023-08-01 20:29:23 +0300

    fix celery config

--- server/config/celery.py
+++ server/config/celery.py
@@ -8,5 +8,6 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.settings')
 app = Celery('config')
 
 app.config_from_object('django.conf:settings', namespace='CELERY')
+app.config_from_object('config.settings.installed_apps')
 
 app.autodiscover_tasks()
