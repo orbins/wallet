c44913b11bfff0ebfb4f27f26f7713794e90371f
Takeshi <orwellj@mail.ru>
2023-08-05 20:15:26 +0300

    change analytics viewset

--- server/apps/goals/models/querysets/goal.py
+++ server/apps/goals/models/querysets/goal.py
@@ -5,3 +5,52 @@ class GoalQuerySet(QuerySet):
 
     def count_uncompleted_goals(self):
         return self.filter(is_completed=False).count()
+
+    def get_analyzed_data(self):
+
+
+    # closest_goals = [goal.days_to_goal for goal in queryset.filter(is_completed=False) if goal.days_to_goal > 0]
+    # if len(closest_goals) > 0:
+    #     closest_goals.sort()
+    #     most_closest_goal = closest_goals[GoalConstants.CLOSEST_GOAL_INDEX]
+    # else:
+    #     most_closest_goal = None
+    #
+    # categories = TransactionCategory.objects.filter(
+    #     goals__in=queryset.filter(is_completed=True)
+    # ).annotate_with_goals_counter().order_by('goals_counter')
+    # if len(categories) > 0:
+    #     most_successful_category = categories[1].name
+    # else:
+    #     most_successful_category = None
+    # # INDEX
+    #
+    # categories = TransactionCategory.objects.filter(
+    #     goals__in=queryset).annotate_with_goals_counter().order_by(
+    #     'goals_counter')
+    # if len(categories) > 0:
+    #     most_popular_category = categories[1].name
+    # else:
+    #     most_popular_category = None
+    # # INDEX
+    #
+    # response = {
+    #     'uncompleted_goals': queryset.count_uncompleted_goals(),
+    #
+    #     'total_invested_amount': Deposit.objects.filter(
+    #         goal__in=queryset.filter(is_completed=False)
+    #     ).aggregate_amount()['total_amount'],
+    #
+    #     'total_percent_amount': Deposit.objects.filter(
+    #         goal__in=queryset,
+    #         refill_type=RefillTypes.FROM_PERCENTS
+    #     ).aggregate_amount()['total_amount'],
+    #
+    #     'percent__amount_this_month': Deposit.objects.filter(
+    #         refill_type=RefillTypes.FROM_PERCENTS,
+    #         created_at__month=timezone.now().month
+    #     ).aggregate_amount()['total_amount'],
+    #
+    #     'most_closest_goal': most_closest_goal,
+    #     'most_successful_category': most_successful_category,
+    #     'most_popular_category': most_popular_category,
--- server/apps/goals/viewsets/goal.py
+++ server/apps/goals/viewsets/goal.py
@@ -6,7 +6,7 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.request import Request
 from rest_framework.response import Response
 
-from ..constants import RefillTypes, GoalConstants
+from ..constants import RefillTypes
 from ..filters import GoalFilter
 from ..models import Goal, Deposit
 from ..serializers import (
@@ -17,7 +17,7 @@ from ..serializers import (
     GoalUpdateSerializer,
 )
 from ...pockets.constants import TransactionTypes
-from ...pockets.models import Transaction, TransactionCategory
+from ...pockets.models import Transaction
 
 
 class GoalViewSet(viewsets.ModelViewSet):
@@ -29,9 +29,9 @@ class GoalViewSet(viewsets.ModelViewSet):
             serializer = GoalCreateSerializer
         elif self.action in ('update', 'partial_update'):
             serializer = GoalUpdateSerializer
-        elif self.action == 'refill_goal':
+        elif self.action == 'refill':
             serializer = DepositCreateSerializer
-        elif self.action == 'complete_goal':
+        elif self.action == 'complete':
             serializer = GoalCompleteSerializer
         else:
             serializer = GoalRetrieveSerializer
@@ -45,9 +45,17 @@ class GoalViewSet(viewsets.ModelViewSet):
 
         return queryset
 
+    def get_object(self):
+        queryset = self.get_queryset()
+        if self.action == 'analyze':
+            obj = self.filter_queryset(queryset).get_analyzed_data()
+        else:
+            obj = super().get_object()
+        return obj
+
     def perform_create(self, serializer):
         user = self.request.user
-        if self.action == 'refill_goal':
+        if self.action == 'refill':
             instance = serializer.save()
             Transaction.objects.create(
                 user=user,
@@ -96,64 +104,15 @@ class GoalViewSet(viewsets.ModelViewSet):
             transaction_date=timezone.now(),
         )
 
-    @action(methods=('POST',), detail=False, url_path='refill')
-    def refill_goal(self, request: Request, *args, **kwargs) -> Response:
+    @action(methods=('POST',), detail=False)
+    def refill(self, request: Request, *args, **kwargs) -> Response:
         return super().create(request, *args, **kwargs)
 
-    @action(methods=('PATCH',), detail=True, url_path='complete')
-    def complete_goal(self, request: Request, *args, **kwargs) -> Response:
+    @action(methods=('PATCH',), detail=True)
+    def complete(self, request: Request, *args, **kwargs) -> Response:
         return super().partial_update(request, *args, **kwargs)
 
-    @action(methods=('GET',), detail=False, url_path='analytics')
-    def analyze_goal(self, request: Request, *args, **kwargs) -> Response:
-        queryset = self.get_queryset()
-
-        closest_goals = [goal.days_to_goal for goal in queryset.filter(is_completed=False) if goal.days_to_goal > 0]
-        if len(closest_goals) > 0:
-            closest_goals.sort()
-            most_closest_goal = closest_goals[GoalConstants.CLOSEST_GOAL_INDEX]
-        else:
-            most_closest_goal = None
-
-        categories = TransactionCategory.objects.filter(
-            goals__in=queryset.filter(is_completed=True)
-        ).annotate_with_goals_counter().order_by('goals_counter')
-        if len(categories) > 0:
-            most_successful_category = categories[1].name
-        else:
-            most_successful_category = None
-        # INDEX
-
-        categories = TransactionCategory.objects.filter(
-            goals__in=queryset).annotate_with_goals_counter().order_by(
-            'goals_counter')
-        if len(categories) > 0:
-            most_popular_category = categories[1].name
-        else:
-            most_popular_category = None
-        # INDEX
-
-        response = {
-            'uncompleted_goals': queryset.count_uncompleted_goals(),
-
-            'total_invested_amount': Deposit.objects.filter(
-                goal__in=queryset.filter(is_completed=False)
-            ).aggregate_amount()['total_amount'],
-
-            'total_percent_amount': Deposit.objects.filter(
-                goal__in=queryset,
-                refill_type=RefillTypes.FROM_PERCENTS
-            ).aggregate_amount()['total_amount'],
-
-            'percent__amount_this_month': Deposit.objects.filter(
-                refill_type=RefillTypes.FROM_PERCENTS,
-                created_at__month=timezone.now().month
-            ).aggregate_amount()['total_amount'],
-
-            'most_closest_goal': most_closest_goal,
-            'most_successful_category': most_successful_category,
-            'most_popular_category': most_popular_category,
-
-        }
-        return Response(response)
+    @action(methods=('GET',), detail=False)
+    def analyze(self, request: Request, *args, **kwargs) -> Response:
+        return super().retrieve(request, *args, **kwargs)
 
